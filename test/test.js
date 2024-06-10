9ee58075f025831238cfc7eb56a9d9659c0e5369
Marcelo <marcelo.san.car@hotmail.com>
2024-06-10 11:28:28 -0300

    server port test

--- test/test.js
+++ test/test.js
@@ -1,10 +1,34 @@
-const regex = /^.+ #\w+:\d+ #ID:[a-zA-Z0-9]{5}$/;
+// Importa os módulos necessários
+const express = require('express');
+const os = require('os');
 
-// Exemplo de string que deve corresponder
-const message = "Olá gostaria de ver as opções! #Mesa:5 #ID:318d5";
+// Cria uma aplicação Express
+const app = express();
 
-if (regex.test(message)) {
-  console.log("A mensagem corresponde ao padrão.");
-} else {
-  console.log("A mensagem não corresponde ao padrão.");
+// Define a porta em que o servidor irá escutar
+const port = 3000;
+
+// Função para obter o endereço IP da máquina
+function getLocalIpAddress() {
+  const interfaces = os.networkInterfaces();
+  for (const name of Object.keys(interfaces)) {
+    for (const iface of interfaces[name]) {
+      // Pula interfaces internas (loopback) e IPv6
+      if (iface.family === 'IPv4' && !iface.internal) {
+        return iface.address;
+      }
+    }
+  }
+  return '127.0.0.1'; // Caso nenhum IP externo seja encontrado
 }
+
+// Define a rota para /home que responde com "Server running!"
+app.get('/home', (req, res) => {
+  res.send('Server running!');
+});
+
+// Inicia o servidor
+app.listen(port, () => {
+  const ipAddress = getLocalIpAddress();
+  console.log(`Server running at http://${ipAddress}:${port}/`);
+});
