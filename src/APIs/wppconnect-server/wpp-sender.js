016668a3c4c0600494593a178413c4d110f5b7cc
Marcelo <marcelo.san.car@hotmail.com>
2024-06-13 20:37:49 -0300

    beta 1.0 (all test done)

--- src/APIs/wppconnect-server/wpp-sender.js
+++ src/APIs/wppconnect-server/wpp-sender.js
@@ -6,7 +6,7 @@ export default WppSender;
 
 const secretKey = config.default.secretKey || "BIXTOKEN";
 const session = "NERDWHATS_AMERICA";
-const path = "54.226.73.247:5001";
+const path = "54.152.116.180:5001";
 
 let token = null;
 let tokenPromise = null;
