2c279aa9268433330971fe4e103e08cf2c6b3dc3
Marcelo <marcelo.san.car@hotmail.com>
2024-07-04 21:21:18 -0300

    config endpoint createChatbot

--- src/routes/message.js
+++ src/routes/message.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { handleMessageRequest } from "../controllers/system/selector.js";
+import { message } from "../controllers/system/selector.js";
 
 const message = express.Router();
 
@@ -10,7 +10,7 @@ message.get("/", (req, res) => {
 message.post("/", async (req, res) => {
   try {
     // console.log('\x1b[33m%s\x1b[0m', 'handleMessageRequest req.body: ', JSON.stringify(req.body, null, 2));
-    const response = await handleMessageRequest(req.body);
+    const response = await message.handleMessageRequest(req.body);
     console.log('\x1b[33m%s\x1b[0m', 'Message response: ', response);
     res.status(200).json('OK');
   } catch (error) {
