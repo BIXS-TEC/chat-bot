10d45ad511a9ec9a2bcfdfc5567ec9731ac6baa9
Marcelo <marcelo.san.car@hotmail.com>
2024-07-08 10:57:43 -0300

    cors server update

--- src/server.js
+++ src/server.js
@@ -2,26 +2,29 @@ import express from 'express';
 import message from './routes/message.js';
 import auth from './routes/auth.js';
 import config from './routes/config.js';
-// import { systemSetup } from './controllers/system/selector.js';
+import cors from 'cors';
 
-// systemSetup();
 const app = express();
 const port = 5002;
 
+const corsOptions = {
+  origin: '*', // Permitir todas as origens
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  preflightContinue: false,
+  optionsSuccessStatus: 204
+};
+
+app.use(cors(corsOptions));
+
 app.use(express.json());
-
 app.use('/message', message);
-
 app.use('/auth', auth);
-
 app.use('/config', config);
 
 app.get('/', (req, res) => {
   res.status(200).json('Home!');
-})
+});
 
 app.listen(port, () => {
   console.log(`Path to chatbot: http://54.227.229.46:5002/message`);
 });
-
-// node_modules\@wppconnect\server\dist\util\createSessionUtil.js
