91d2ba399cc7fd656ec2e6eb8da2ed4e3c35efab
Marcelo <marcelo.san.car@hotmail.com>
2024-07-10 14:03:53 -0300

    session setup

--- src/controllers/init/wpp-server.js
+++ src/controllers/init/wpp-server.js
@@ -1,7 +1,9 @@
 import _interopRequireDefault from '@babel/runtime/helpers/interopRequireDefault';
-import _config from '@wppconnect/server/dist/config';
-import { initServer as _initServer } from '@wppconnect/server/dist/index';
+import _config from '@wppconnect/server/dist/config.js'; // Especificando o arquivo .js
+import { initServer as _initServer } from '@wppconnect/server/dist/index.js'; // Especificando o arquivo .js
+
+const config = _interopRequireDefault(_config).default;
 
 export const initServer = () => {
-  return _initServer(_interopRequireDefault(_config).default);
+  return _initServer(config);
 };
